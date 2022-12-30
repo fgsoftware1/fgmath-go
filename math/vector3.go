b0bfb84c58c9f1f60bb349d5143f2a5faeeeba5c
Gabriel Aguiar <fgsoftwarestudio@gmail.com>
2022-12-30 16:10:57 +0000

    setting up vector3
    Signed-off-by: Gabriel Aguiar <fgsoftwarestudio@gmail.com>

---
+++ math/vector3.go
@@ -0,0 +1,13 @@
+package Vector3
+
+type Vector3 struct {
+	X, Y, Z float64
+}
+
+func New(x float64, y float64, z float64) *Vector3 {
+	return &Vector3{X: x, Y: y, Z: z}
+}
+
+func Dot(a *Vector3, b *Vector3) float64 {
+	return a.X*b.X + a.Y*b.Y + a.Z*b.Z
+}
