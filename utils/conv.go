138a1f1b46c6129c942164be34a788a37942f43a
Gabriel Aguiar <fgsoftwarestudio@gmail.com>
2022-12-30 15:52:55 +0000

    add degree conversions
    Signed-off-by: Gabriel Aguiar <fgsoftwarestudio@gmail.com>

--- utils/conv.go
+++ utils/conv.go
@@ -1,7 +1,19 @@
 package conv
 
-import "strconv"
+import (
+	"strconv"
+
+	"github.com/fgsoftware1/gomath/constants"
+)
 
 func FloatToString(f float64) string {
 	return strconv.FormatFloat(f, 'f', 6, 64)
+}
+
+func DegToRad(angle float64) float64 {
+	return angle*constants.Pi/180
+}
+
+func RadToDeg(angle float64) float64 {
+	return angle*180/constants.Pi
 }
